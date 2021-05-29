d0ecc242f0028dfc76194027cb8a29570f05b7c5
Shiv Sagar Shah <71564583+ShivSagarShah@users.noreply.github.com>
2021-05-30 01:17:46 +0530

    Update CB.cpp

--- CB.cpp
+++ CB.cpp
@@ -1,18 +1,18 @@
 #include<bits/stdc++.h>
 #include<string.h>
 using namespace std;
-string move(string s){ //recursion humko ek character ko chhodke string ko ache se arrange kar dega
+string move(string s){
     if(s.length() == 0){ // agar hum last tak pauch gaye to khaali string return kara denge
         string b = "";
         return b;
     }
 
-    string a = s.substr(0,1); // hum pehle element ko pakdenge aur dekhenge ki vo kya hai, agar x hai to jo bhi ans aayega recursion se uske last mein laga denge
-    if(a == "x"){
-        s = move(s.substr(1)) + a;        
+    // hum pehle element ko pakdenge aur dekhenge ki vo kya hai, agar x hai to jo bhi ans aayega recursion se uske last mein laga denge
+    if(s.substr(0,1) == "x"){
+        s = move(s.substr(1)) + s.substr(0,1);        
     }
     else{
-        s = a + move(s.substr(1)); //aur agar x nhi hai to pehle laga denge jo bhi ans aayega recursion se
+        s = s.substr(0,1) + move(s.substr(1)); //aur agar x nhi hai to pehle laga denge jo bhi ans aayega recursion se
     }
     return s;
 }
